ed1a2de10d6eca417cd22ed1e72e16c150f13b3f
Robin Lake <mister.lake@gmail.com>
2017-03-19 12:07:10 -0600

    Math functions complete; error handling improved; double operator presses accounted for

--- docs/javascript.js
+++ docs/javascript.js
@@ -1,78 +1,130 @@
-var stored = '0';
-var operator = '+';
-var display = document.getElementById("displayText").innerHTML;
+var stored = '';
+var operator = '';
+var toExecute = '';
+var display = '';
 
 // Button press functions
 function b1(){
     display += "1" ;
+    toExecute += "1";
     document.getElementById("displayText").innerHTML = display;
 }
 function b2(){
     display += "2" ;
+    toExecute += "2";
     document.getElementById("displayText").innerHTML = display;
 }
 function b3(){
     display += "3" ;
+    toExecute += "3";
     document.getElementById("displayText").innerHTML = display;
 }
 function b4(){
-    console.log('foo');
-}
+    display += "4" ;
+    toExecute += "4";
+    document.getElementById("displayText").innerHTML = display;}
 function b5(){
-    console.log('foo');
-}
+    display += "5" ;
+    toExecute += "5";
+    document.getElementById("displayText").innerHTML = display;}
 function b6(){
-    console.log('foo');
-}
+    display += "6" ;
+    toExecute += "6";
+    document.getElementById("displayText").innerHTML = display;}
 function b7(){
-    console.log('foo');
-}
+    display += "7" ;
+    toExecute += "7";
+    document.getElementById("displayText").innerHTML = display;}
 function b8(){
-    console.log('foo');
-}
+    display += "8" ;
+    toExecute += "8";
+    document.getElementById("displayText").innerHTML = display;}
 function b9(){
-    console.log('foo');
-}
+    display += "9" ;
+    toExecute += "9";
+    document.getElementById("displayText").innerHTML = display;}
 function b0(){
-    console.log('foo');
-}
+    display += "0" ;
+    toExecute += "0";
+    document.getElementById("displayText").innerHTML = display;}
 function bCE(){
-    console.log('foo');
+    display = '';
+    toExecute = '';
+    document.getElementById("displayText").innerHTML = display;    
 }
 function bAC(){
-    console.log('foo');
-}
+    display = '';
+    stored = '';
+    toExecute = '';
+    document.getElementById("displayText").innerHTML = display; }
 function bBack(){
-    console.log('foo');
+    display = display.slice(0,-1);
+    document.getElementById("displayText").innerHTML = display;    
 }
 function bTimes(){
     operator = '*';
-    display = '0';
+    if(toExecute == '' || /[^-*+/]/.test(toExecute.slice(-1))){
+    toExecute += '*';}
+    display = '';
+    document.getElementById("displayText").innerHTML = '';
 }
 function bDivide(){
     operator = '/';
-    display = '0';
+    if(toExecute == '' || /[^-*+/]/.test(toExecute.slice(-1))){
+    toExecute += '/';}
+    display = '';
+    document.getElementById("displayText").innerHTML = '';
 }
 function bPlus(){
     operator = '+';
-    display = '0';
+    if(toExecute == '' || /[^-*+/]/.test(toExecute.slice(-1))){
+    toExecute += '+';}
+    display = '';
+    document.getElementById("displayText").innerHTML = '';
 }
 function bMinus(){
     operator = '-';
-    display = '0';
+    if(toExecute == '' || /[^-*+/]/.test(toExecute.slice(-1))){
+    toExecute += '-';}
+    display = '';
+    document.getElementById("displayText").innerHTML = '';
 }
 function bDot(){
-    console.log('.');
-}
-function bEquals(){
+    display += "." ;
+    document.getElementById("displayText").innerHTML = display;}
+/* function bEquals(){
     if (operator !== ''){
-        stored = eval(parseInt(stored, 10) + operator +  parseInt(display, 10)); /* performs the calculation */
-        display = stored;
-    } else {
+        //stored = (stored).valueOf();
+        //toExecute = toExecute.valueOf();
+        console.log(stored);
+        console.log(toExecute);
+        document.getElementById("displayText").innerHTML = '';
+        stored = eval(stored + toExecute);
+        //stored = eval(+stored + operator +  +display); // performs the calculation 
+        display = '';
+    } else { // needs to be corrected so clearing entries don't erase memory 
+       if(display !== ''){
         stored = display;
     }
-    document.getElementById("displayText").innerHTML = stored; /* updated display screen */
+        display = '';
+    }
+    document.getElementById("displayText").innerHTML = stored; // updated display screen 
     operator = '';
-    console.log(stored);
-}
+    toExecute = '';
+} */
+function bEquals() {
+    if(operator !== '' & /[0-9]/.test(toExecute)){
+    var result = eval(stored.valueOf() + toExecute.valueOf());
+    document.getElementById("displayText").innerHTML = result ;
+    stored = result;
+    display = '';
+    toExecute = '';
+    operator = '';
+    } else {
+        stored = toExecute;
+        toExecute = '';
+        display = '';
+        operator = '';
+    }
 
+}
